d4abcdb00dc8b7d12e950645e63a155ddcdeb655
Abraham Elmahrek <abraham@elmahrek.com>
2013-09-24 18:49:12 -0700

    Use transient name ID instead of persistent.

--- djangosaml2/views.py
+++ djangosaml2/views.py
@@ -41,6 +41,7 @@ except ImportError:
 from saml2 import BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
 from saml2.client import Saml2Client
 from saml2.metadata import entity_descriptor
+from saml2.saml import NAMEID_FORMAT_TRANSIENT
 
 from djangosaml2.cache import IdentityCache, OutstandingQueriesCache
 from djangosaml2.cache import StateCache
@@ -114,7 +115,7 @@ def login(request,
     try:
         (session_id, result) = client.prepare_for_authenticate(
             entityid=selected_idp, relay_state=came_from,
-            binding=BINDING_HTTP_REDIRECT
+            binding=BINDING_HTTP_REDIRECT, nameid_format=NAMEID_FORMAT_TRANSIENT
             )
     except TypeError, e:
         logger.error('Unable to know which IdP to use')
